b9f25f1f324d9b2f0df5a453458ba4e3f092ba7b
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-04-16 17:46:55 +0100

    Added decent looking version of the landing page
    ref https://linear.app/tryghost/issue/MOM-14/create-a-basic-version-of-the-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
-const baseFontSize = 10
-
 module.exports = {
   content: ["home.hbs"],
   theme: {
     extend: {
-      shadows: {
+      boxShadow: {
         'juicy': '0px 100px 80px 0px rgba(0, 0, 0, 0.03), 0px 41.778px 33.422px 0px rgba(0, 0, 0, 0.02), 0px 22.336px 17.869px 0px rgba(0, 0, 0, 0.02), 0px 12.522px 10.017px 0px rgba(0, 0, 0, 0.01), 0px 6.65px 5.32px 0px rgba(0, 0, 0, 0.01), 0px 2.767px 2.214px 0px rgba(0, 0, 0, 0.01)'
       }
     },
