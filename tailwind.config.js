899e921faff8baee5e3753f50d2543d349e6431a
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-04-16 14:39:03 +0100

    Added ugly first version of the landing page
    ref https://linear.app/tryghost/issue/MOM-14/create-a-basic-version-of-the-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,103 @@
 /** @type {import('tailwindcss').Config} */
+
+const baseFontSize = 10
+
 module.exports = {
   content: ["home.hbs"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: () => ({
+        ...Array.from({ length: 96 }, (_, index) => index * 0.5)
+          .filter((i) => i)
+          .reduce(
+            (acc, i) => ({ ...acc, [i]: `${i / (baseFontSize / 4)}rem` 		 }),
+            {}
+          ),
+      }),
+      fontSize: {
+        xs: [
+          `${(16 * 0.75) / baseFontSize}rem`, /* 12px */
+          {
+            lineHeight: `${(16 * 1) / baseFontSize}rem` /* 16px */,
+          },
+        ],
+        sm: [
+          `${(16 * 0.875) / baseFontSize}rem`, /* 14px */
+          {
+            lineHeight: `${(16 * 1.25) / baseFontSize}rem` /* 20px */,
+          },
+        ],
+        base: [
+          `${(16 * 1) / baseFontSize}rem`, /* 16px */
+          {
+            lineHeight: `${(16 * 1.5) / baseFontSize}rem` /* 24px */,
+          },
+        ],
+        lg: [
+          `${(16 * 1.125) / baseFontSize}rem`, /* 18px */
+          {
+            lineHeight: `${(16 * 1.75) / baseFontSize}rem` /* 28px */,
+          },
+        ],
+        xl: [
+          `${(16 * 1.25) / baseFontSize}rem`, /* 20px */
+          {
+            lineHeight: `${(16 * 1.75) / baseFontSize}rem` /* 28px */,
+          },
+        ],
+        "2xl": [
+          `${(16 * 1.5) / baseFontSize}rem`, /* 24px */
+          {
+            ineHeight: `${(16 * 2) / baseFontSize}rem` /* 32px */,
+          },
+        ],
+        "3xl": [
+          `${(16 * 1.875) / baseFontSize}rem`, /* 30px */
+          {
+            lineHeight: `${(16 * 2.25) / baseFontSize}rem` /* 36px */,
+          },
+        ],
+        "4xl": [
+          `${(16 * 2.25) / baseFontSize}rem`, /* 36px */
+          {
+            lineHeight: `${(16 * 2.5) / baseFontSize}rem` /* 40px */,
+          },
+        ],
+        "5xl": [
+          `${(16 * 3) / baseFontSize}rem`, /* 48px */
+          {
+            lineHeight: (16 * 1) / baseFontSize,
+          },
+        ],
+        "6xl": [
+          `${(16 * 3.75) / baseFontSize}rem`, /* 60px */
+          {
+            lineHeight: (16 * 1) / baseFontSize,
+          },
+        ],
+        "7xl": [
+          `${(16 * 4.5) / baseFontSize}rem`, /* 72px */
+          {
+            lineHeight: (16 * 1) / baseFontSize,
+          },
+        ],
+        "8xl": [
+          `${(16 * 6) / baseFontSize}rem`, /* 96px */
+          {
+            lineHeight: (16 * 1) / baseFontSize,
+          },
+        ],
+        "9xl": [
+          `${(16 * 8) / baseFontSize}rem`, /* 128px */
+          {
+            lineHeight: (16 * 1) / baseFontSize,
+          },
+        ],
+      },
+      shadows: {
+        'juicy': '0px 100px 80px 0px rgba(0, 0, 0, 0.03), 0px 41.778px 33.422px 0px rgba(0, 0, 0, 0.02), 0px 22.336px 17.869px 0px rgba(0, 0, 0, 0.02), 0px 12.522px 10.017px 0px rgba(0, 0, 0, 0.01), 0px 6.65px 5.32px 0px rgba(0, 0, 0, 0.01), 0px 2.767px 2.214px 0px rgba(0, 0, 0, 0.01)'
+      }
+    },
   },
   plugins: [],
 }
-
