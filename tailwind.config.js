c14eae994c215b3e24d9181c03efcd9ce60f1804
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-04-17 17:23:35 +0100

    Added signup forms to the landing page
    closes https://linear.app/tryghost/issue/MOM-15/add-signup-forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["home.hbs"],
+  content: ["home.hbs","partials/email-subscription.hbs"],
   theme: {
     fontFamily: {
       'sans-serif': ['"Inter Display"', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
@@ -10,6 +10,9 @@ module.exports = {
     extend: {
       boxShadow: {
         'juicy': '0px 100px 80px 0px rgba(0, 0, 0, 0.03), 0px 41.778px 33.422px 0px rgba(0, 0, 0, 0.02), 0px 22.336px 17.869px 0px rgba(0, 0, 0, 0.02), 0px 12.522px 10.017px 0px rgba(0, 0, 0, 0.01), 0px 6.65px 5.32px 0px rgba(0, 0, 0, 0.01), 0px 2.767px 2.214px 0px rgba(0, 0, 0, 0.01)'
+      },
+      maxWidth: {
+        '8xl': '90rem',
       }
     },
   },
