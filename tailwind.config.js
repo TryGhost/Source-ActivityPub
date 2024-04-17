e5e365c8908893bb311a39e4777f6e7e60fe7535
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-04-17 19:41:32 +0100

    Added Youtube to the bar chart
    closes https://linear.app/tryghost/issue/MOM-36/add-youtube-to-the-bar-chart

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,19 @@
 module.exports = {
   content: ["home.hbs","partials/email-subscription.hbs"],
   theme: {
-    fontFamily: {
-      'sans-serif': ['"Inter Display"', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-      'serif': ['"Ivar Text"']
-    },
     extend: {
       boxShadow: {
         'juicy': '0px 100px 80px 0px rgba(0, 0, 0, 0.03), 0px 41.778px 33.422px 0px rgba(0, 0, 0, 0.02), 0px 22.336px 17.869px 0px rgba(0, 0, 0, 0.02), 0px 12.522px 10.017px 0px rgba(0, 0, 0, 0.01), 0px 6.65px 5.32px 0px rgba(0, 0, 0, 0.01), 0px 2.767px 2.214px 0px rgba(0, 0, 0, 0.01)'
       },
       maxWidth: {
         '8xl': '90rem',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        'sans-serif': ['"Inter Display"', 'ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+        'serif': ['"Ivar Text"']
       }
     },
   },
